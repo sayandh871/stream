7ba0e1119a859b6487ab416176ea57d2f4e5ffd3
sayandh <sayandh871@gmail.com>
2025-11-17 23:45:24 +0530

    database model created

---
+++ backend/src/models/meeting.model.js
@@ -0,0 +1,9 @@
+import mongoose, { Schema } from "mongoose";
+
+const meetingSchema = new Schema({
+  user_id: { type: String },
+  meetingCode: { type: String, required: true },
+  date: { type: Date, default: Date.now, required: true },
+});
+
+export const Meeting = mongoose.model("Meeting", meetingSchema);
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,10 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  token: { type: String },
+});
+
+export const User = mongoose.model("User", userSchema);
